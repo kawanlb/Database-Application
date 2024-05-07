112fd7e3cb8ca7752b073736392f3399a0c49cd8
Matheus Emanuel <emanuelmatheus948@gmail.com>
2024-05-07 11:09:45 -0300

    commit

--- python/mysql_to_redis.py
+++ python/mysql_to_redis.py
@@ -26,6 +26,59 @@ def insert_data_redis_batch(chave_redis, data):
     print("Dados inseridos com sucesso no Redis.")
     return True
 
+def ranking_vendas():
+    r = connect_to_redis()
+    conn = connect_to_db()
+
+    cursor = conn.cursor()
+    cursor.execute(""" 
+        SELECT CONCAT(j.titulo,' - ', p.plataforma_nome) AS jogo_plataforma,
+        SUM(v.vendas_na+v.vendas_eu+v.vendas_jp+v.outras_vendas) AS total_vendas FROM jogo j
+        INNER JOIN jogo_editora je ON j.jogo_id =je.jogo_id 
+        INNER JOIN jogo_plataforma jp ON je.jogo_editora_id =jp.jogo_editora_id 
+        INNER JOIN venda v ON jp.jogo_plataforma_id = v.jogo_plataforma_id 
+        INNER JOIN plataforma p ON jp.plataforma_id = p.plataforma_id
+        GROUP BY CONCAT(j.titulo, ' - ', p.plataforma_nome) 
+        ORDER BY total_vendas ASC
+    """)
+    resultado = cursor.fetchall()
+    cursor.close()
+
+    if resultado:
+        r.delete("ranking_vendas")
+        for row in resultado:
+            jogo_plataforma = row[0]
+            total_vendas = float(row[1])  # Convertendo Decimal para float
+            r.zadd("ranking_vendas", {jogo_plataforma: total_vendas})
+            print("Ranking de vendas criado e armazenado no Redis.")
+    else:
+        print("Não há dados de vendas para criar o ranking.")
+
+def jogo_genero(genero_id):
+    r = connect_to_redis()
+    conn = connect_to_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+            SELECT j.titulo, g.genero_nome
+            FROM jogo j
+            INNER JOIN genero g ON j.genero_id = g.genero_id 
+            WHERE g.genero_id = %s
+        """, (genero_id,))
+    
+    resultados = cursor.fetchall()
+    conn.close()
+
+    if resultados:
+        nome_genero = resultados[0][1].lower()  # Obtém o nome do gênero
+        conjunto_key = f"genero:{nome_genero}"
+        for jogo, _ in resultados:
+            r.sadd(conjunto_key, jogo)
+            print("adicionado com sucesso genero")
+        
+    else:
+        print("Não há dados de jogos para inserir no Redis.")
+
 try:
     conn = connect_to_db()
     if not conn:
@@ -37,10 +90,10 @@ try:
     # Consultas ao MySQL
     cursor.execute("SELECT titulo FROM jogo")
     resultados_jogos = cursor.fetchall()
+
     cursor.execute("SELECT editora_nome FROM editora")
     resultados_editoras = cursor.fetchall()
-    cursor.execute("SELECT plataforma.plataforma_nome, jogo.titulo FROM jogo_plataforma INNER JOIN plataforma ON jogo_plataforma.plataforma_id = plataforma.plataforma_id INNER JOIN jogo ON jogo_plataforma.jogo_editora_id = jogo.jogo_id")
-    resultados_plataformas_jogos = cursor.fetchall()
+
     cursor.execute("SELECT jogo.titulo, venda.vendas_na, venda.vendas_eu, venda.vendas_jp, venda.outras_vendas FROM venda INNER JOIN jogo_plataforma ON venda.jogo_plataforma_id = jogo_plataforma.jogo_plataforma_id INNER JOIN jogo ON jogo_plataforma.jogo_editora_id = jogo.jogo_id")
     resultados_vendas = cursor.fetchall()
 
@@ -55,10 +108,6 @@ try:
         nomes_editoras = [editora[0] for editora in resultados_editoras]
         insert_data_redis_batch("nomes_editoras", nomes_editoras)
 
-    if resultados_plataformas_jogos:
-        dados_plataformas_jogos = [(plataforma_jogo[0], plataforma_jogo[1]) for plataforma_jogo in resultados_plataformas_jogos]
-        insert_data_redis_batch("plataformas_jogos", dados_plataformas_jogos)
-
     if resultados_vendas:
         vendas_na = [float(venda[1]) for venda in resultados_vendas]
         vendas_eu = [float(venda[2]) for venda in resultados_vendas]
@@ -72,3 +121,10 @@ try:
 
 except Exception as e:
     print("Erro:", e)
+
+
+ranking_vendas()
+#Sports
+jogo_genero(1)
+#Action
+jogo_genero(9)
